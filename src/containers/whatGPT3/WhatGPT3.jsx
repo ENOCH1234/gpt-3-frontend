3ee310117cb1851be78373813eebb20e1afbbb40
Olayode Enoch <olayodeenoch@gmail.com>
2023-10-19 22:46:10 +0100

    working on what GPT3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,15 @@ import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+      <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+          <div className="gpt3__whatgpt3-feature">
+              <Feature />
+          </div>
+          <div className="gpt3__whatgpt3-header">
+              <h1 className="gradient-text"></h1>
+          </div>
+        
+    </div>
   )
 }
 
