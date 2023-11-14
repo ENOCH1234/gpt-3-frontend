4634d0184e6e4eb6460521ac32c8d72da0033b3e
Olayode Enoch <olayodeenoch@gmail.com>
2023-11-15 00:30:55 +0100

    Started work on what is gpt-3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
       <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
           <div className="gpt3__whatgpt3-feature">
-              <Feature />
+              <Feature title="What is GPT-3" text="Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here." />
           </div>
-          <div className="gpt3__whatgpt3-header">
-              <h1 className="gradient-text"></h1>
+          <div className="gpt3__whatgpt3-heading">
+              <h1 className="gradient__text">
+                  The possibilities are beyond your imagination
+              </h1>
+              <p>Explore The Library</p>
+          </div>
+          <div className="gpt3__whatgpt3-container">
+              <Feature title="Chatbots" text="Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here " />
+              <Feature title="Knowledgebase" text="Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here" />
+              <Feature title="Education" text="Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here " />
           </div>
         
     </div>
