3961aa9f377859fc07cb43e7709509ccc983b305
Olayode Enoch <olayodeenoch@gmail.com>
2023-11-15 10:53:15 +0100

    Completed features

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { Feature } from '../../components';
 import './features.css';
+
+const featuresData = [
+    {
+        title: 'Improving end distrusts instantly',
+        text: 'Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here'
+    },
+    {
+        title: 'Become the tended active',
+        text: 'Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here'
+    },
+    {
+        title: 'Message or aim nothing',
+        text: 'Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here'
+    },
+    {
+        title: 'Really boy law county',
+        text: 'Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here'
+    }
+]
 
 const Features = () => {
   return (
     <div className="gpt3__features section__padding" id="features">
         <div className="gpt3__features-heading">
             <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into the Future Today and Make it Happen.</h1>
-        </div>
+            <p>Request Early Access to Get Started</p>
+          </div>
+          <div className="gpt3__features-container">
+              {featuresData.map((item, index) => (
+                  <Feature title={item.title} text={item.text} />
+              ))}
+          </div>
     </div>
   )
 }
