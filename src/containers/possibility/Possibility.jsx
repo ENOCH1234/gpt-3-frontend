85bdc5087a5a96a1351f108a795375781f6ba8ef
Olayode Enoch <olayodeenoch@gmail.com>
2023-11-15 11:29:29 +0100

    Completed possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import possibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className="gpt3__possibility section__padding" id="possibility">
+        <div className="gpt3__possibility-image">
+            <img src={possibilityImage} alt="possibility" />
+        </div>
+        <div className="gpt3__possibility-content">
+            <h4>Request Early Access to Get Started</h4>
+            <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+            <p>Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here Anything I want to put here.</p>
+            <h4>Request Early Access to Get Started</h4>
+        </div>
+    </div>
   )
 }
 
-export default Possibility
+export default Possibility;
